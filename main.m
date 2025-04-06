e35b1162254a66661029fdfec4138dce6a5c9117
maloch18 <mdlochridge@gmail.com>
2025-04-06 13:17:03 -0500

    Added main operation script.

---
+++ main.m
@@ -0,0 +1,13 @@
+addpath("Codes\","Configuration\","Operation\");
+
+measurements = {};
+
+camera = serialport("COM4",2400,"FlowControl","hardware","Timeout",30)
+enter_remote_mode(camera);
+model = parse_data(measure(camera,111));
+camera.Tag = model.model;
+
+
+
+exit_remote_mode(camera);
+clear camera;
