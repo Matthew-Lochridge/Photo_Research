787ed2018873da9d0e0e2f4763eedcec99e236bb
Matthew-Lochridge <mdlochridge@gmail.com>
2025-07-10 13:37:59 -0500

    removed old implements

--- test.m
+++
@@ -1,6 +0,0 @@
-[camera,status] = initialize_camera('COM4',20)
-data = struct();
-% [data, error_code] = send_command(camera,data,'M',601,[],[],[])
-response = writeread(camera,'M601'); % '00000,0,-1,-1,-1,0,1,0,3,1,1,2,0.00'
-[~,~] = send_command(camera,[],'Q',[],[],[],[]);
-clear camera;
